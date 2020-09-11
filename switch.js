75f24862719b2124c989b6f501506fb606671eb7
JimmyLv <jimmy.jinglv@gmail.com>
2020-09-11 10:31:20 +0800

    added switch mode button css

--- switch.js
+++ switch.js
@@ -1,26 +1,37 @@
 document.querySelector('html').classList = 'card-mode'
 
-const appendButton = function (innerText, clickHandler) {
-  let targetElement = document.querySelector('.bp3-icon-menu+div')
+const appendButton = function (type, clickHandler) {
+  const innerText = {
+    cardList: 'Card List',
+    editCard: 'Edit Card',
+    document: 'Document'
+  }[type]
+  let targetElement = document.querySelector('.roam-topbar [style="flex: 1 1 0px;"]')
   targetElement.style.color = 'green'
   targetElement.style.display = 'flex'
   targetElement.style.justifyContent = 'center'
 
   const button = document.createElement('button')
   button.innerHTML = innerText
+  button.className = 'mode-button' + ' ' + type
   button.onclick = clickHandler
 
   targetElement.appendChild(button)
 }
 
-appendButton('card', function () {
+appendButton('cardList', function () {
   document.querySelector('html').classList = 'card-mode'
+  document.querySelector('.zoom-path-view.rm-zoom').firstChild.click()
+})
+
+appendButton('editCard', function () {
+  document.querySelector('html').classList = 'edit-mode'
+  let selected = document.querySelector('textarea[autocapitalize="none"]')
+  if (selected) {
+    selected.parentNode.parentNode.querySelector('.simple-bullet-inner').click()
+  }
 })
 
 appendButton('document', function () {
   document.querySelector('html').classList = 'document-mode'
-})
-
-appendButton('list', function () {
-  document.querySelector('html').classList = 'list-mode'
 })
