7293c543bf7e6c2acbe8e6ad1712e3fa5f9344f6
JimmyLv <jimmy.jinglv@gmail.com>
2020-09-14 17:15:28 +0800

    fix: updated the script helper function

--- js/index.js
+++ js/index.js
@@ -1,25 +1,19 @@
-/* global  loadKeyEvents, loadTypeAhead, loadJumpNav, jumpToDateComponent, rmQuickRefenceSystem, device, displayStartup */
-
-const disabledFeatures = typeof window.disabledFeatures !== 'undefined' ? window.disabledFeatures : [];
-
-function addScriptToPage(tagId, script) {
-  addElementToPage(Object.assign(document.createElement('script'),{src:script}) , tagId, 'text/javascript')
+function appendScriptToPage(tagId, script) {
+  appendElementToPage(Object.assign(document.createElement('script'), { src: script }), tagId, 'text/javascript')
 }
 
-function addModuleToPage(tagId, script) {
-  addElementToPage(Object.assign(document.createElement('script'),{src:script}) , tagId, 'module')
+function appendCSSToPage(tagId, cssToAdd) {
+  appendElementToPage(Object.assign(document.createElement('link'), {
+    href: cssToAdd,
+    rel: 'stylesheet',
+  }), tagId, 'text/css')
 }
 
-function addCSSToPage(tagId, cssToAdd) {
-  addElementToPage(Object.assign(document.createElement('link'),{href:cssToAdd, rel: 'stylesheet'} ) , tagId, 'text/css')
-}
-
-function addElementToPage(element, tagId, typeT ) {
-  try { document.getElementById(tagId).remove() } catch(e){}  //Delete any existing reference
-  if(disabledFeatures && disabledFeatures.indexOf(tagId) > -1) { return } //Exit if disabled
-  Object.assign(element, { type:typeT, async:false, tagId:tagId } )
+function appendElementToPage(element, tagId, typeT) {
+  try { document.getElementById(tagId).remove() } catch (e) {}  //Delete any existing reference
+  Object.assign(element, { type: typeT, async: false, tagId: tagId })
   document.getElementsByTagName('head')[0].appendChild(element)
 }
 
-addCSSToPage( 'cssCard',     window.URLScriptServer + 'css/card.min.css' )
-addScriptToPage( 'switchMode'  ,       window.URLScriptServer + 'js/switch.js'         )
+appendCSSToPage('cssCard', window.URLScriptServer + 'css/card.min.css')
+appendScriptToPage('switchMode', window.URLScriptServer + 'js/switch.js')
