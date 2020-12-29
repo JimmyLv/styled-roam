576ba6b411baf23acb2c474a1e79194953c0301e
JimmyLv <jimmy.jinglv@gmail.com>
2020-12-29 17:46:29 +0800

    feat: inject the css directly

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ setTimeout(() => {
   initCardifyTheme();
 }, 3000);
 
+function appendCSSToPage(tagId, cssToAdd) {
+  appendElementToPage(
+    Object.assign(document.createElement("link"), {
+      href: cssToAdd,
+      rel: "stylesheet",
+    }),
+    tagId,
+    "text/css"
+  );
+}
+
+function appendElementToPage(element, tagId, typeT) {
+  try {
+    document.getElementById(tagId).remove();
+  } catch (e) {} //Delete any existing reference
+  Object.assign(element, { type: typeT, async: false, tagId: tagId });
+  document.getElementsByTagName("head")[0].appendChild(element);
+}
+
+appendCSSToPage("cssCalendar", window.URLScriptServer + "js/index.css");
+
 if (module.hot) {
   module.hot.dispose(function () {
     console.log("模块即将被替换时");
