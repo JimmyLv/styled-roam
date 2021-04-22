9e7beb25477a453b875497181700a1ca2a7f574b
JimmyLv <jimmy.jinglv@gmail.com>
2021-04-22 18:31:58 +0800

    fix: added config

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,10 @@
 const { promisify } = require('util')
-const path = require('path');
+const path = require('path')
 const dateFormat = require('dateformat')
 const readFileAsync = promisify(require('fs').readFile)
 
 // Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
-const TEMPLATE_DIR = '/lib/assets/templates'
+const TEMPLATE_DIR = './lib/assets/templates'
 
 // the *.hbs template and partials should be passed as strings of contents
 const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
@@ -13,8 +13,27 @@ const commitTemplate = readFileAsync(path.join(TEMPLATE_DIR, 'commit-template.hb
 module.exports = {
   branches: ['master', 'dev'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+      },
+    ],
     [
       '@semantic-release/changelog',
       {
