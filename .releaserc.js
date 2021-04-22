139cf9abbb4fff0ae24af98bceebfab35132f15c
JimmyLv <jimmy.jinglv@gmail.com>
2021-04-22 18:27:56 +0800

    feat: add auto release with emoji

---
+++ .releaserc.js
@@ -0,0 +1,67 @@
+const { promisify } = require('util')
+const path = require('path');
+const dateFormat = require('dateformat')
+const readFileAsync = promisify(require('fs').readFile)
+
+// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
+const TEMPLATE_DIR = '/lib/assets/templates'
+
+// the *.hbs template and partials should be passed as strings of contents
+const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
+const commitTemplate = readFileAsync(path.join(TEMPLATE_DIR, 'commit-template.hbs'))
+
+module.exports = {
+  branches: ['master', 'dev'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['docs/CHANGELOG.md'],
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'src',
+            label: 'styled-roam',
+          },
+        ],
+        addReleases: 'top',
+      },
+    ],
+    [
+      'semantic-release-gitmoji',
+      {
+        releaseRules: {
+          major: [':boom:'],
+          minor: [':sparkles:'],
+          patch: [':bug:', ':ambulance:', ':lock:'],
+        },
+        releaseNotes: {
+          template,
+          partials: { commitTemplate },
+          helpers: {
+            datetime: function (format = 'UTC:yyyy-mm-dd') {
+              return dateFormat(new Date(), format)
+            },
+          },
+          issueResolution: {
+            template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+            baseUrl: 'https://github.com',
+            source: 'github.com',
+          },
+        },
+      },
+    ],
+  ],
+}
