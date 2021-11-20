786416fc52ab30e1434fcbdc1684ccedab35102c
JimmyLv <jimmy.jinglv@gmail.com>
2021-11-21 00:40:13 +0800

    feat: polished tree table view styling

--- src/views/card-flow.js
+++ src/views/card-flow.js
@@ -1,23 +1,12 @@
 import { appendIcon, switchTo } from '../utils/dom-helper'
 import addHotKeys from '../utils/hotkey'
+import { updateCardsStickyTop } from '../utils/update-cards-sticky-top'
 import './card-flow.less'
-
-function updateCardsStickyTop() {
-  setTimeout(() => {
-    const cards = document.querySelectorAll('.rm-block__children.rm-level-0 > .roam-block-container');
-
-    console.log({ cards });
-
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].style.top = String(130 + (i - 1) * 70) + 'px';
-    }
-  }, 500)
-}
 
 export default function initCardFlowMode() {
   appendIcon('cardFlow', 'heat-grid', function () {
     switchTo('flow-mode')
-    updateCardsStickyTop();
+    updateCardsStickyTop()
   })
 
   window.addEventListener('locationchange', updateCardsStickyTop)
--- src/views/tree-table.js
+++ src/views/tree-table.js
@@ -1,10 +1,12 @@
 import { appendIcon, switchTo } from '../utils/dom-helper'
 import addHotKeys from '../utils/hotkey'
 import './tree-table.less'
+import { updateCardsStickyTop } from '../utils/update-cards-sticky-top';
 
 export default function initTreeTableMode() {
   appendIcon('treeTable', 'th', function () {
     switchTo('table-mode')
+    updateCardsStickyTop('0px')
   })
 
   addHotKeys({
